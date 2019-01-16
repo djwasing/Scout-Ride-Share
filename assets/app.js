b12237206d78d89ed2da2ca616bd5873b47008b4
Don Wasinger <DonWasinger@Dons-MacBook-Pro.local>
2019-01-15 19:10:49 -0600

    google maps search bar

--- assets/app.js
+++ assets/app.js
@@ -12,56 +12,21 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-//-----------------------------------------
+// //-----------------------------------------
 
-// get the google maps url and API key from firebase
+// // get the google maps url and API key from firebase
 
-$(document).ready(function() {
-    database.ref().orderByChild("dateAdded").on("child_added", function (snapshot) {
-    var data = snapshot.val();
-    var mapKey = data.url;
-    console.log(mapKey);
-    $("#googleMapsHead").attr("src", mapKey);
-    //need to not run the showGoogleMaps func until the data is retrieved
-    //from the firebase. ›
-    var dataReceived = false
-    
-    });
-});
-
+// database.ref().orderByChild("dateAdded").on("child_added", function (snapshot) {
+//     var data = snapshot.val();
+//     var mapKey = data.url;
+//     console.log(mapKey);
+//     $("#googleMapsHead").attr("src", mapKey);
+//     //need to not run the showGoogleMaps func until the data is retrieved
+//     //from the firebase. ›
+// });
+//$.when(getAPI()).done(showGoogleMaps);
 
 //------------------------------------------
-
-
-
-var position = [38.899154, -94.726126];
-
-function showGoogleMaps() {
-
-    var latLng = new google.maps.LatLng(position[0], position[1]);
-
-    var mapOptions = {
-        zoom: 16, // initialize zoom level - the max value is 21
-        streetViewControl: false, // hide the yellow Street View pegman
-        scaleControl: true, // allow users to zoom the Google Map
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        center: latLng
-    };
-
-    map = new google.maps.Map(document.getElementById('googlemaps'),
-        mapOptions);
-
-    // Show the default red marker at the location
-    marker = new google.maps.Marker({
-        position: latLng,
-        map: map,
-        draggable: false,
-        animation: google.maps.Animation.DROP
-    });
-}
-
-google.maps.event.addDomListener(window, 'load', showGoogleMaps);
-
 
 //   This example adds a search box to a map, using the Google Place Autocomplete
 //   feature. People can enter geographical searches. The search box will return a
@@ -70,12 +35,11 @@ google.maps.event.addDomListener(window, 'load', showGoogleMaps);
 //   This example requires the Places library. Include the libraries=places
 //   parameter when you first load the API. For example:
 
-
 function initAutocomplete() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -33.8688, lng: 151.2195 },
-        zoom: 13,
-        mapTypeId: 'roadmap'
+    var map = new google.maps.Map(document.getElementById('googlemaps'), {
+      center: {lat: -33.8688, lng: 151.2195},
+      zoom: 13,
+      mapTypeId: 'roadmap'
     });
 
     // Create the search box and link it to the UI element.
@@ -84,62 +48,59 @@ function initAutocomplete() {
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
     // Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function () {
-        searchBox.setBounds(map.getBounds());
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
     });
 
     var markers = [];
     // Listen for the event fired when the user selects a prediction and retrieve
     // more details for that place.
-    searchBox.addListener('places_changed', function () {
-        var places = searchBox.getPlaces();
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
 
-        if (places.length == 0) {
-            return;
+      if (places.length == 0) {
+        return;
+      }
+
+      // Clear out the old markers.
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
         }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
 
-        // Clear out the old markers.
-        markers.forEach(function (marker) {
-            marker.setMap(null);
-        });
-        markers = [];
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
 
-        // For each place, get the icon, name and location.
-        var bounds = new google.maps.LatLngBounds();
-        places.forEach(function (place) {
-            if (!place.geometry) {
-                console.log("Returned place contains no geometry");
-                return;
-            }
-            var icon = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25)
-            };
-
-            // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-                map: map,
-                icon: icon,
-                title: place.name,
-                position: place.geometry.location
-            }));
-
-            if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-            } else {
-                bounds.extend(place.geometry.location);
-            }
-        });
-        map.fitBounds(bounds);
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
     });
 }
-
-
-
 
 
 
