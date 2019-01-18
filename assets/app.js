63d5ba5fdc81f8d87c8a44b355ce471a5f5dc9e7
Don Wasinger <DonWasinger@Dons-MacBook-Pro.local>
2019-01-17 19:35:33 -0600

    uber work

--- assets/app.js
+++ assets/app.js
@@ -1,17 +1,16 @@
-
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyC1tZ0V983FSdADD8TTjq7bjLC0AaR_HA4",
-    authDomain: "project1-ac08d.firebaseapp.com",
-    databaseURL: "https://project1-ac08d.firebaseio.com",
-    projectId: "project1-ac08d",
-    storageBucket: "",
-    messagingSenderId: "611644065697"
+  apiKey: "AIzaSyC1tZ0V983FSdADD8TTjq7bjLC0AaR_HA4",
+  authDomain: "project1-ac08d.firebaseapp.com",
+  databaseURL: "https://project1-ac08d.firebaseio.com",
+  projectId: "project1-ac08d",
+  storageBucket: "",
+  messagingSenderId: "611644065697"
 };
 firebase.initializeApp(config);
 
 var database = firebase.database();
-var map, infoWindow, pos, startLatitude, startLongitude, endLatitude, endLongitude, queryURLETA, queryURLPrice;
+var map, infoWindow, pos, startLatitude, startLongitude, endLatitude, endLongitude;
 
 
 // //-----------------------------------------
@@ -38,100 +37,100 @@ var map, infoWindow, pos, startLatitude, startLongitude, endLatitude, endLongitu
 //   parameter when you first load the API. For example:
 
 function initAutocomplete() {
-    map = new google.maps.Map(document.getElementById('googlemaps'), {
-      center: {lat: 40, lng: -100},
-      zoom: 5,
-      mapTypeId: 'roadmap'
+  map = new google.maps.Map(document.getElementById('googlemaps'), {
+    center: {lat: 40, lng: -100},
+    zoom: 5,
+    mapTypeId: 'roadmap'
+  });
+  infoWindow = new google.maps.InfoWindow;
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
     });
-    infoWindow = new google.maps.InfoWindow;
-    // Create the search box and link it to the UI element.
-    var input = document.getElementById('pac-input');
-    var searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    markers = [];
 
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function() {
-      searchBox.setBounds(map.getBounds());
-    });
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
 
-    var markers = [];
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-    searchBox.addListener('places_changed', function() {
-      var places = searchBox.getPlaces();
-
-      if (places.length == 0) {
+    
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
         return;
       }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+      //console.log("newLat: " + place.geometry.location.lat());
+      //console.log("newLng: " + place.geometry.location.lng());
+      endLatitude = (place.geometry.location.lat());
+      endLongitude = (place.geometry.location.lng());
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
 
-      // Clear out the old markers.
-      markers.forEach(function(marker) {
-        marker.setMap(null);
-      });
-      markers = [];
-
-      // For each place, get the icon, name and location.
-      var bounds = new google.maps.LatLngBounds();
-  
-      
-      places.forEach(function(place) {
-        if (!place.geometry) {
-          console.log("Returned place contains no geometry");
-          return;
-        }
-        var icon = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25)
-        };
-        //console.log("newLat: " + place.geometry.location.lat());
-        //console.log("newLng: " + place.geometry.location.lng());
-        endLatitude = (place.geometry.location.lat());
-        endLongitude = (place.geometry.location.lng());
-        // Create a marker for each place.
-        markers.push(new google.maps.Marker({
-          map: map,
-          icon: icon,
-          title: place.name,
-          position: place.geometry.location
-        }));
-
-        if (place.geometry.viewport) {
-          // Only geocodes have viewport.
-          bounds.union(place.geometry.viewport);
-        } else {
-          bounds.extend(place.geometry.location);
-        }
-      });
-      map.fitBounds(bounds);
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
     });
+    map.fitBounds(bounds);
+  });
 }
 
 // Try HTML5 geolocation.
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      
-      infoWindow.setPosition(pos);
-      infoWindow.setContent("Your Location");
-      infoWindow.open(map);
-      map.setCenter(pos);
-      startLatitude = (pos.lat);        
-      startLongitude = (pos.lng);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
+  navigator.geolocation.getCurrentPosition(function(position) {
+    pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
     
+    infoWindow.setPosition(pos);
+    infoWindow.setContent("Your Location");
+    infoWindow.open(map);
+    map.setCenter(pos);
+    startLatitude = (pos.lat);        
+    startLongitude = (pos.lng);
+  }, function() {
+    handleLocationError(true, infoWindow, map.getCenter());
+  });
+  
 } 
 
 else {
-  // Browser doesn’t support Geolocation
-  handleLocationError(false, infoWindow, map.getCenter());
+// Browser doesn’t support Geolocation
+handleLocationError(false, infoWindow, map.getCenter());
 }
 
 
@@ -144,90 +143,83 @@ else {
 //   infoWindow.open(map);
 // }
 
-//--------UBER API info----------------
+//UBER API info
 
-// make funciton for getting a token
-// THEN make function for each estimate request
-  
-  
 
-  
+
 //Calling the function when uber button is clicked
-  $(document).ready(function() {
+$(document).ready(function() {
+  
+  $("#uberBtn").click(function(e) {
+    e.preventDefault();
+    
+    console.log("startLat: " + startLatitude);
+    console.log("startLng: " + startLongitude);
+    console.log("endLat: " + endLatitude);
+    console.log("endLng: " + endLongitude);
+ 
+    var queryURLETA = "https://api.uber.com/v1.2/estimates/time?start_latitude=" + startLatitude + "&start_longitude=" + startLongitude + "&end_latitude=" + endLatitude + "&end_longitude=" + endLongitude + "&server_token=CYeYg4Brhv5cRtRYESfcC9iRKG9TCDCfZhxASEaS";
+    var queryURLPrice = "https://api.uber.com/v1.2/estimates/price?start_latitude=" + startLatitude + "&start_longitude=" + startLongitude + "&end_latitude=" + endLatitude + "&end_longitude=" + endLongitude + "&server_token=CYeYg4Brhv5cRtRYESfcC9iRKG9TCDCfZhxASEaS";
+    // console.log("THis is the URL for ETA " + queryURLETA);
 
-  
-    
-    $("#uberBtn").click(function(e) {
-      e.preventDefault();
-      queryURLETA = "https://api.uber.com/v1.2/estimates/time?start_latitude=" + startLatitude + "&start_longitude=" + startLongitude + "&end_latitude=" + endLatitude + "&end_longitude=" + endLongitude + "&server_token=CYeYg4Brhv5cRtRYESfcC9iRKG9TCDCfZhxASEaS";
-      queryURLPrice = "https://api.uber.com/v1.2/estimates/price?start_latitude=" + startLatitude + "&start_longitude=" + startLongitude + "&end_latitude=" + endLatitude + "&end_longitude=" + endLongitude + "&server_token=CYeYg4Brhv5cRtRYESfcC9iRKG9TCDCfZhxASEaS";
-      console.log("startLat: " + startLatitude);
-      console.log("startLng: " + startLongitude);
-      console.log("endLat: " + endLatitude);
-      console.log("endLng: " + endLongitude);
-      //console.log(queryURLPrice);
-  
-  //Function for Uber AJAX Prices-------
-  
-    function uberTestPrice() {
 
-    jQuery.ajax({
-                type: "GET",
-                url: queryURLPrice, 
-                crossDomain: true,
-                beforeSend: setHeader, 
-                 
-    
-            }).then(function(response){
-                //console.log(response);
-    
-                //Looping through all the prices objects
-                for ( var i = 0; i < response.prices.length; i++){
+
+//Function for Uber AJAX Prices
+
+  function uberTestPrice() {
+
+  jQuery.ajax({
+              type: "GET",
+              url: queryURLPrice, 
+              crossDomain: true,
+              beforeSend: setHeader, 
+               
+  
+          }).then(function(response){
+              console.log("Price");
+              
+              //Looping through all the prices objects
+              for ( var i = 0; i < response.prices.length; i++){
+              
+              
+              console.log(response.prices[i].low_estimate);
+  
+              }
+          });
+  
+  }
+  //Function calling the Uber ETA for Times
+  function uberTestETA() {
+  
+  jQuery.ajax({
+              type: "GET",
+              url: queryURLETA, 
+              crossDomain: true,
+              beforeSend: setHeader, 
+              
+  
+          }).then(function(response){
+              console.log("ETA");
                 
-                
-                console.log(response.prices[i].low_estimate);
-    
-                }
-            });
-    
-    }
-    //Function calling the Uber ETA for Times
-    function uberTestETA() {
-    
-    jQuery.ajax({
-                type: "GET",
-                url: queryURLETA, 
-                crossDomain: true,
-                beforeSend: setHeader, 
-                 
-    
-            }).then(function(response){
-                //console.log(response);
-    
-                 //Looping through all the ETA objects
-                 for ( var i = 0; i < response.times.length; i++){
-                    console.log(response.times[i].estimate);
-                 }
-            });
-    
-    }
-    //Function to set the header for the authorization key
-    function setHeader(xhr) {
-    
-        //database.ref().on("value", function(snapshot){
-          var token = "JA.VUNmGAAAAAAAEgASAAAABwAIAAwAAAAAAAAAEgAAAAAAAAG8AAAAFAAAAAAADgAQAAQAAAAIAAwAAAAOAAAAkAAAABwAAAAEAAAAEAAAAPwqOP3m-X0rOweEw5hGnhpsAAAAAu6sKlqtlNn30XjyGBffSHfoUnLbYVhKFQcRXS3EsOgY7X1rC_bcnc8ys9_yi_DvgGIqeWZGLX7j5Nb7A0fA85B19ya4zALR9Asjb5bAzB7PTtNMTZ6Ok7vJ1zeWlkMcXzcjmJ9KNcwUhNXpDAAAAG2oTu7jIRek50SxxCQAAABiMGQ4NTgwMy0zOGEwLTQyYjMtODA2ZS03YTRjZjhlMTk2ZWU";
-    
-          xhr.setRequestHeader("Authorization", + token);
-          xhr.setRequestHeader("Access-Control-Allow-Origin","*");
-    
-        //})
-    }
-    uberTestPrice();
-    uberTestETA();
-    });
-  }); 
-
-    
-    
+               //Looping through all the ETA objects
+               for ( var i = 0; i < response.times.length; i++){
+                  console.log(response.times[i].estimate);
+               }
+          });
   
+  }
+  //Function to set the header for the authorization key
+  function setHeader(xhr) {
     
+    database.ref().on("value", function(snapshot){
+      var token = (snapshot.val().Uber.UberKey);
+      xhr.setRequestHeader("Authorization", + token);
+      xhr.setRequestHeader("Access-Control-Allow-Origin","*");
+
+    })
+}
+
+uberTestPrice();
+uberTestETA();
+});
+}); 
